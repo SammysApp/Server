c0db8d4567e06e206c0facc4e649166a4180b2c5
Natanel Niazoff <nniazoff@icloud.com>
2019-04-17 15:34:44 -0400

    Add version to socket path

--- Sources/App/Core/sockets.swift
+++ Sources/App/Core/sockets.swift
@@ -4,5 +4,5 @@ public func sockets(_ socketServer: WebSocketServer) {
     guard let socketServer = socketServer as? NIOWebSocketServer else { return }
     
     let orderSessionController = OrderSessionController.default
-    socketServer.get("orderSessions", SocketSession.ID.parameter, use: orderSessionController.add)
+    socketServer.get(AppConstants.version, "orderSessions", SocketSession.ID.parameter, use: orderSessionController.add)
 }
