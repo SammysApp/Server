2a222eb8d9c7108eb67129369352d8f512ca7cab
Natanel Niazoff <nniazoff@icloud.com>
2019-05-13 10:21:08 -0400

    Fix migrations

--- Sources/App/Core/configure.swift
+++ Sources/App/Core/configure.swift
@@ -57,6 +57,9 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     // Configure migrations.
     var migrations = MigrationConfig()
     
+    migrations.add(migration: Availability.self, database: .psql)
+    migrations.add(migration: OrderProgress.self, database: .psql)
+    
     migrations.add(model: StoreHours.self, database: .psql)
     migrations.add(model: Category.self, database: .psql)
     migrations.add(model: Item.self, database: .psql)
@@ -76,8 +79,6 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     migrations.add(model: PurchasedOrderOffer.self, database: .psql)
     migrations.add(model: User.self, database: .psql)
     
-    migrations.add(migration: Availability.self, database: .psql)
-    migrations.add(migration: OrderProgress.self, database: .psql)
     migrations.add(migration: AddDefaultData.self, database: .psql)
     
     services.register(migrations)
